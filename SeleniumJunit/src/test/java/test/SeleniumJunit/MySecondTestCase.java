942abff90d24c8e36d7f977e73e28e5de81c4163
vishalattray <vishalattray@192.168.0.16>
2019-10-31 21:24:40 -0400

    s

--- SeleniumJunit/src/test/java/test/SeleniumJunit/MySecondTestCase.java
+++ SeleniumJunit/src/test/java/test/SeleniumJunit/MySecondTestCase.java
@@ -7,6 +7,7 @@ import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class MySecondTestCase {
 
@@ -28,7 +29,41 @@ public class MySecondTestCase {
 
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		//invoke .exe file
+		System.setProperty("webdriver.chrome.driver", "/Users/vishalattray/eclipse-workspace/Vishal-Selenium/Drivers/chromedriver-3");
+			
+		//Create driver object for chrome browser
+		//Class name = ChromeDriver
+		ChromeDriver  driver = new ChromeDriver();
+				 
+		//Implement the method of web drive
+				 
+		//Go to the url
+		driver.get("https://www.findmyfare.com/");
+				 
+		//Validate your page title is correct
+		System.out.println(driver.getTitle());
+		
+		//Validate landed url
+		System.out.println(driver.getCurrentUrl());
+		
+		//Print page source
+		System.out.println(driver.getPageSource());
+		
+		//Go to the url
+		driver.get("https://www.airbnb.com/");
+		
+		//Navigate back
+		driver.navigate().back();
+		
+		//Navigate forward
+		driver.navigate().forward();
+		
+		//Close current browser
+		driver.close();
+		
+		//Close all the browsers
+		driver.quit();
 	}
 
 }
