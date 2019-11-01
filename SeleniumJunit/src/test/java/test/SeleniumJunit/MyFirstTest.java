f125284fefb8a5f5d6dd4790320e76ebcec05c5f
vishalattray <vishalattray@192.168.0.16>
2019-10-31 20:55:34 -0400

    r

---
+++ SeleniumJunit/src/test/java/test/SeleniumJunit/MyFirstTest.java
@@ -0,0 +1,58 @@
+package test.SeleniumJunit;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import static org.testng.Assert.assertTrue;
+
+import org.junit.Assert.*;
+import org.junit.Test;
+
+
+public class MyFirstTest {
+
+	@Test
+	public void firstTest() {
+
+		//invoke .exe file
+		System.setProperty("webdriver.chrome.driver", "/Users/vishalattray/eclipse-workspace/Vishal-Selenium/Drivers/chromedriver-3");
+			
+		//Create driver object for chrome browser
+		//Class name = ChromeDriver
+		ChromeDriver  driver = new ChromeDriver();
+				 
+		//Implement the method of web drive
+				 
+		//Go to the url
+		driver.get("https://www.findmyfare.com/");
+				 
+		//Validate your page title is correct
+		System.out.println(driver.getTitle());
+		
+		//Validate landed url
+		System.out.println(driver.getCurrentUrl());
+		
+		//Print page source
+		System.out.println(driver.getPageSource());
+		
+		//Go to the url
+		driver.get("https://www.airbnb.com/");
+		
+		//Navigate back
+		driver.navigate().back();
+		
+		//Navigate forward
+		driver.navigate().forward();
+		
+		//Close current browser
+		driver.close();
+		
+		//Close all the browsers
+		driver.quit();
+		
+		assertTrue(true);
+
+	}
+	
+	
+
+}
